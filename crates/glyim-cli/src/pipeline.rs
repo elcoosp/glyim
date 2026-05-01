a7156e625e3d6d0234df8387e714419e2990d8fa
elcoosp <elcoosp@gmail.com>
2026-05-01 11:05:54 +0200

    feat: standard prelude with include_str!, replacing inline string

--- crates/glyim-cli/src/pipeline.rs
+++ crates/glyim-cli/src/pipeline.rs
@@ -157,26 +157,7 @@ pub fn build(input: &Path, output: Option<&Path>) -> Result<PathBuf, PipelineErr
     Ok(output)
 }
 
-const PRELUDE: &str = "\
-pub enum Option<T> {
-    Some(T),
-    None,
-}
-
-pub enum Result<T, E> {
-    Ok(T),
-    Err(E),
-}
-
-extern {
-    fn glyim_alloc(size: i64) -> *mut u8;
-    fn glyim_free(ptr: *mut u8);
-    fn __size_of<T>() -> i64;
-    fn __ptr_offset(ptr: *mut u8, offset: i64) -> *mut u8;
-    fn write(fd: i64, buf: *const u8, len: i64) -> i64;
-    fn abort();
-}
-";
+const PRELUDE: &str = include_str!("../../../stdlib/src/prelude.g");
 
 #[tracing::instrument(name = "run", skip_all)]
 pub fn run(input: &Path) -> Result<i32, PipelineError> {
