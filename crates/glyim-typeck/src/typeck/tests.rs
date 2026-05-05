3f5a91f09c7a695221875e1e6423accc3cfa262b
elcoosp <elcoosp@gmail.com>
2026-05-05 11:07:02 +0200

    test: typeck tests

--- crates/glyim-typeck/src/typeck/tests.rs
+++ crates/glyim-typeck/src/typeck/tests.rs
@@ -1602,7 +1602,7 @@ fn unify_types_param_not_in_list() {
         &type_params,
         &mut sub,
     );
-    assert!_eq!(sub.get(&t), Some(&HirType::Named(u)));
+    assert_eq!(sub.get(&t), Some(&HirType::Named(u)));
 }
 
 #[test]
