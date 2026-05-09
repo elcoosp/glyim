ceada01f4bfed91e40893668607c361c34d07617
elcoosp <elcoosp@gmail.com>
2026-05-09 19:42:05 +0200

    chore: fmt

--- crates/glyim-hir/src/monomorphize/rewrite.rs
+++ crates/glyim-hir/src/monomorphize/rewrite.rs
@@ -390,7 +390,9 @@ impl<'a> MonoContext<'a> {
                     } else {
                         *enum_name
                     }
-                } else { *enum_name };
+                } else {
+                    *enum_name
+                };
                 HirExpr::EnumVariant {
                     id: *id,
                     enum_name: new_enum_name,
